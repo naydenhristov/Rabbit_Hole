2913bcd486ebc52c79905f0b1f0f29a0a51b3c75
Nayden Hristov <n_hristov@mail.bg>
2017-04-15 14:37:32 +0300

    Rabbit Hole

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # Rabbit_Hole
-Descrition in ReadMe
+Descrition of the Task
+
+Rabbit hole is a term for a portal for time travel. 
+You are trying to go back in the past to prevent Kennedy’s assassination. But first you should find the rabbit hole. Your energy is limited and if you exhaust it, you will be tired and can’t continue the mission.
+You will be given an array of strings representing different obstacles you should overcome. 
+
+1.	“Left|[integer value]”-you should jump to the left with [integer value] positions and decrease your energy with [integer value];
+
+2.	“Right|[integer value]”-you should jump to the right with [integer value] positions and decrease your energy with [integer value];
+
+3.	“Bomb|[integer value]”-the bomb explodes and this element of the array should be removed, your energy should be decreased be the [integer value], you should start from the beginning (index 0);
+
+4.	“RabbitHole” – you have found the rabbit hole, the program should stop here, print on the console – “You have 5 years to save Kennedy!”
+ 
+Example input:
+Right|1 Right|10 Left|10 RabbitHole Right|1 Left|2
+100
+
+If your jump is outside of the bonds of the array, you should continue from the beggining. If you are at index 1 and command is Right|10 -> you must jump 5 times to the end and then anothe 5 times from the beginning.
+
+Your mission begins at the first element of the array. The rabbit hole will be only one.
+The program ends when you find the rabbit hole or when your energy is gone. And if it is gone due to bomb explosion you should print “You are dead due to bomb explosion!” on the console or if it is due jumps print
+“You are tired. You can't continue the mission.”.
+After every move someone puts a bomb at the end of the array with [integer value] of your current energy (the last element is removed and the bomb is placed there, but when the last element is the “RabbitHole”, it can’t be removed and the bomb is placed after it).
+
+Input / Constrains
+- First line – space separated string values consists the obstacles you should overcome.
+- Second line – integer value representing your initial energy in range [0-100].
+
+Output
+- If you find the Rabbit Hole you should print: “You have 5 years to save Kennedy!”;
+- If you die due to bomb explosion (your energy is gone, after a bomb explosion): “You are dead due to bomb explosion!”;
+- If you are tired (your energy is gone, after a jump): “You are tired. You can't continue the mission.” .
+
+Examples:
+
+Input:
+RabbitHole Left|100 Right|10 Bomb|10 Left|11
+100
+
+Output:
+You have 5 years to save Kennedy!
+----------------------------------------------
+
+Input:
+Right|100 Left|10 Bomb|11 RabbitHole Bomb|10 Left|2
+80
+
+Output:
+You are tired. You can't continue the mission.
+----------------------------------------------
+
+Input:
+Right|1 Right|10 Left|10 RabbitHole Right|1 Left|2
+100
+
+Output:
+You are dead due to bomb explosion!
+----------------------------------------------
+
+Input:
+Right|1 Left|3 Left|10 RabbitHole Left|1 Left|2
+100
+
+Output:
+You have 5 years to save Kennedy!
---
+++ Rabbit_Hole.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2_1_Rabbit_Hole
+{
+    public class Rabbit_Hole
+    {
+        public static void Main()
+        {
+            List<string> inputList = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+
+            int energy = int.Parse(Console.ReadLine());
+
+            List<string> command = new List<string>();
+            List<int> changeEnergy = new List<int>();
+
+            InitializeLists(inputList, command, changeEnergy);
+
+            int currentIndex = 0;
+
+            RunCommand(command, changeEnergy, currentIndex, energy);
+        }
+
+        private static void InitializeLists(List<string> inputList, 
+            List<string> command, List<int> changeEnergy)
+        {
+            foreach (string element in inputList)
+            {
+                string[] initLists = element.Split('|').ToArray();
+
+                if (initLists[0] == "RabbitHole")
+                {
+                    command.Add(initLists[0]);
+                    changeEnergy.Add(0);
+                }
+                else
+                {
+                    command.Add(initLists[0]);
+                    changeEnergy.Add(Convert.ToInt32(initLists[1]));
+                }
+            }
+        }
+
+        private static void RunCommand(List<string> command, List<int> changeEnergy, 
+            int currentIndex, int energy)
+        {
+            int length = command.Count;
+            
+            switch (command[currentIndex])
+            {
+                case "RabbitHole" :
+                    Console.WriteLine("You have 5 years to save Kennedy!");
+                    break;
+                
+                case "Left" :
+                    energy -= changeEnergy[currentIndex];
+                    
+                    currentIndex -= changeEnergy[currentIndex] % length;
+                    
+                    if (currentIndex < 0)
+                    {
+                        currentIndex += length;
+                    }
+                   
+                    if (energy <= 0)
+                    {
+                        Console.WriteLine("You are tired. You can't continue the mission.");
+                    }
+                    else if (command[length - 1] == "RabbitHole")
+                    {
+                        AddBomb(command, changeEnergy, energy);
+                    }
+                    else 
+                    {
+                        ReplaceLastWithBomb(command, changeEnergy, energy, length);
+                        RunCommand(command, changeEnergy, currentIndex, energy);
+                    }
+                    break;
+                
+                case "Right" :
+                    energy -= changeEnergy[currentIndex];
+
+                    currentIndex += changeEnergy[currentIndex] % length;
+
+                    if (currentIndex > length - 1)
+                    {
+                        currentIndex -= length;
+                    }
+                    
+                    if (energy <= 0)
+                    {
+                        Console.WriteLine("You are tired. You can't continue the mission.");
+                    }
+                    else if (command[length - 1] == "RabbitHole")
+                    {
+                        AddBomb(command, changeEnergy, energy);
+                    }
+                    else
+                    {
+                        ReplaceLastWithBomb(command, changeEnergy, energy, length);
+                        RunCommand(command, changeEnergy, currentIndex, energy);
+                    }
+                    break;
+                
+                case "Bomb" :
+                    energy -= changeEnergy[currentIndex];
+                    
+                    if (energy <= 0)
+                    {
+                        Console.WriteLine("You are dead due to bomb explosion!");
+                    }
+                    else
+                    {
+                        command.RemoveAt(currentIndex);
+                        changeEnergy.RemoveAt(currentIndex);
+                        currentIndex = 0;
+                        RunCommand(command, changeEnergy, currentIndex, energy);
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+
+        private static void ReplaceLastWithBomb(List<string> command, List<int> changeEnergy, int energy, int length)
+        {
+            command.RemoveAt(length - 1);
+            changeEnergy.RemoveAt(length - 1);
+            command.Insert(length - 1, "Bomb");
+            changeEnergy.Insert(length - 1, energy);
+        }
+
+        private static void AddBomb(List<string> command, List<int> changeEnergy, int energy)
+        {
+            command.Add("Bomb");
+            changeEnergy.Add(energy);
+        }
+    }
+}
